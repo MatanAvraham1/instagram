9ee77543a1047e7a7ef8543af462e75199219510
Matan Avraham <matan03123321@gmail.com>
2022-05-12 17:59:20 +0300

    Deletes the old Server and keeps the new clean architecture one

--- Server/app.js
+++ Server/app.js
@@ -1,29 +1,11 @@
 require('dotenv').config()
-const express = require('express')
 const mongoose = require('mongoose')
+const { startServer } = require('./Adapters/RestApi')
 
-
-// Connects to the database
+// Connects to the database (currently using mongodb)
 mongoose.connect(process.env.DATABASE_URL).then((result) => { console.log("Connected to db!") }).catch((err) => {
     console.log(err)
 })
 
 
-// Creates the express app
-const app = express()
-
-app.use(express.json())
-
-const { authRouter } = require('./routes/user/auth_route')
-app.use('/api/', authRouter)
-
-const { userRouter } = require('./routes/user/user_route')
-app.use('/api/users/', userRouter)
-
-// const { chatRouter } = require('./routes/chat/chat_route')
-// app.use('/api/chats/', chatRouter)
-
-
-app.listen(5000, () => {
-    console.log("Listening on port 5000...")
-})
+startServer()
